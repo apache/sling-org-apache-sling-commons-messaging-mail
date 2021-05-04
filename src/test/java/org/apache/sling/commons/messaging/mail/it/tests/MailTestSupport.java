768172f73d108ad2b23f4c6c1561efc3d3342a36
Oliver Lietz <olli@apache.org>
2021-05-04 10:32:39 +0200

    SLING-10017 Switch to Jakarta Mail and Activation APIs
    * Update Jakarta Mail and Activation to 2.0.1
    * Add com.sun.activation:jakarta.activation (required?)
    * Use org.apache.servicemix.specs.activation-api-2.0.1 SNAPSHOT (SM-4760)
    * Remove obsolete Commons Email dependency
    * Update to GreenMail 2.0.0-alpha-1

--- src/test/java/org/apache/sling/commons/messaging/mail/it/tests/MailTestSupport.java
+++ src/test/java/org/apache/sling/commons/messaging/mail/it/tests/MailTestSupport.java
@@ -65,8 +65,9 @@ public abstract class MailTestSupport extends TestSupport {
             // Sling Commons Messaging Mail
             testBundle("bundle.filename"),
             mavenBundle().groupId("org.apache.sling").artifactId("org.apache.sling.commons.messaging").versionAsInProject(),
-            mavenBundle().groupId("jakarta.activation").artifactId("jakarta.activation-api").versionAsInProject(),
+            mavenBundle().groupId("org.apache.servicemix.specs").artifactId("org.apache.servicemix.specs.activation-api-2.0.1").versionAsInProject(),
             mavenBundle().groupId("jakarta.mail").artifactId("jakarta.mail-api").versionAsInProject(),
+            mavenBundle().groupId("com.sun.activation").artifactId("jakarta.activation").versionAsInProject(),
             mavenBundle().groupId("com.sun.mail").artifactId("jakarta.mail").versionAsInProject(),
             mavenBundle().groupId("org.apache.commons").artifactId("commons-lang3").versionAsInProject(),
             scr(),
